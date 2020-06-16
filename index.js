bf3d47ee8083292fe8cef1647e3b4b24607b1532
geraldos <sepdwijaya18@gmail.com>
2020-06-16 16:18:00 +0700

    update isi

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ title.style.textAlign = 'center'
 const ingredients = document.getElementById('section-two');
 ingredients.innerHTML = `
 <p>
+    <h1>Bahan - Bahan</h1>
     <ul>
         <li>10 udang</li>
         <li>1 wortel besar</li>
@@ -17,7 +18,21 @@ ingredients.innerHTML = `
 </p>
 `
 
-const images = document.getElementById('section-three');
+const makes = document.getElementById('section-three');
+makes.innerHTML = `
+<p>
+    <h1>Cara Membuat</h1>
+    <ul>
+        <li>Cuci sayur dan potong-potong</li>
+        <li>Tumis bawang putih sampai wangi, masukkan jahe cincang dan udang sampai berubah warna</li>
+        <li>Masukkan wortel dan air secukupnya, lalu semua bumbu lainnya, cek rasa</li>
+        <li>Masukkan semua sayuran lainnya kecuali brokoli. Jangan masak terlalu lama, kira-kira sayur sudah setengah matang.</li>
+        <li>Terakhir masukkan brokoli aduk sebentar lalu larutan maizena. Aduk sampai air berubah agak kental, matikan api.</li>
+    </ul>
+</p>
+`
+
+const images = document.getElementById('section-four');
 images.innerHTML = `
 <img src="./resepCapcay.jpg">
 `
