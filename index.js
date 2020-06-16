3bebf4946fb4d55b95d3d3d84d8de547e9db18b7
geraldos <sepdwijaya18@gmail.com>
2020-06-16 16:54:19 +0700

    update jadi array

--- index.js
+++ index.js
@@ -2,35 +2,14 @@ const title = document.getElementById('section-one');
 title.innerHTML = `<h1>Resep Capcay Goreng</h1>`;
 title.style.textAlign = 'center'
 
-const ingredients = document.getElementById('section-two');
-ingredients.innerHTML = `
-<p>
-    <h1>Bahan - Bahan</h1>
-    <ul>
-        <li>10 udang</li>
-        <li>1 wortel besar</li>
-        <li>1 brokoli kecil</li>
-        <li>1/2 kembang kol</li>
-        <li>Sedikit Jamur kuping</li>
-        <li>Sedikit Kapri</li>
-        <li>Sedikit jagung muda</li>
-    </ul>
-</p>
-`
+const ingredients1 = ['10 udang', '1 wortel besar', '1/2 kembang kol', '1/2 kembang kol',
+    'Sedikit Jamur kuping', 'Sedikit Kapri', 'Sedikit jagung muda'
+];
+ingredients1.forEach(myFunction);
 
-const makes = document.getElementById('section-three');
-makes.innerHTML = `
-<p>
-    <h1>Cara Membuat</h1>
-    <ul>
-        <li>Cuci sayur dan potong-potong</li>
-        <li>Tumis bawang putih sampai wangi, masukkan jahe cincang dan udang sampai berubah warna</li>
-        <li>Masukkan wortel dan air secukupnya, lalu semua bumbu lainnya, cek rasa</li>
-        <li>Masukkan semua sayuran lainnya kecuali brokoli. Jangan masak terlalu lama, kira-kira sayur sudah setengah matang.</li>
-        <li>Terakhir masukkan brokoli aduk sebentar lalu larutan maizena. Aduk sampai air berubah agak kental, matikan api.</li>
-    </ul>
-</p>
-`
+function myFunction(item, index) {
+    document.getElementById("demo").innerHTML += `<li>${item}</li>`;
+}
 
 const images = document.getElementById('section-four');
 images.innerHTML = `
